b3559e1d99557741ed71e6e1e61a74e05593a00f
tinn <229813475@qq.com>
2023-04-23 21:50:43 +0800

    首页

--- pages/tabBar/home/Home.vue
+++ pages/tabBar/home/Home.vue
@@ -1,11 +1,21 @@
 <template>
-	<view class="flex">
-		home
+	<view class="flex fd-c">
+		<view class="status_bar">
+			<!-- 这里是状态栏 -->
+			导航栏
+		</view>
+		<view>轮播图</view>
+		<view>课程列表</view>
 	</view>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss">
+	.status_bar {
+		height: 90px;
+		width: 100%;
+		background: #f8f8f8;
+	}
 </style>
