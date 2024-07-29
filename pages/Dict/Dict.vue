eecd87ed6e2da4be9b6b4c1d65ccd08c3b100b74
douqiting <229813475@qq.com>
2024-07-29 21:23:01 +0800

    字典查询业务逻辑功能完善

--- pages/Dict/Dict.vue
+++ pages/Dict/Dict.vue
@@ -1,11 +1,11 @@
 <template>
 	<view class="dict-page flex fd-c">
 		<view class="search-box">
-			<input class="input" @input="getInput" type="text">
-			<view class="search-btn">icon</view>
+			<input class="input" @input="getInput" @confirm="search" v-model="queryWordValue" type="text" search="查询">
+			<view class="search-clear-btn" @click="clearSearchValue">clear</view>
 		</view>
 		<view class="search-container">
-			<view class="tips-box" v-if="tipsVisible">
+			<view class="tips-box" v-if="blockVisible === blockVisibleConfig.tips">
 				<view v-for="item in tips.list">
 					<view class="tip flex">
 						<view class="tip-word">{{item.word}}</view>
@@ -13,7 +13,39 @@
 					</view>
 				</view>
 			</view>
-			<view class="search-result" v-else></view>
+			<view class="search-history" v-else-if="blockVisible === blockVisibleConfig.history">
+				<view class="flex fw-w">
+					<view class="history-item" @click="searchByHistory(item.word)" v-for="item in queryHistory">
+						{{ item.word }}
+					</view>
+				</view>
+				<view class="clear-btn">清空搜索历史</view>
+			</view>
+			<view class="search-result" v-else>
+				<view class='pronounce flex jc-sb'>
+					<view class="flex ai-c">美 <image class="speaker-icon" src="../../static/images/dict/speaker.png" mode=""></image></view>
+					<view class="flex ai-c">英 <image class="speaker-icon" src="../../static/images/dict/speaker.png" mode=""></image></view>
+					<view>搜藏</view>
+				</view>
+				<view class="translate">
+					<view v-for="item in queryResult.trans">
+						{{item}}
+					</view>
+				</view>
+				<view class="phrases">
+					<view v-for="item in queryResult.phrases">
+						<view>{{item.content}}</view>
+						<view>{{item.cn}}</view>
+					</view>
+				</view>
+				<view class="synos"></view>
+				<view class="sentences">
+					<view v-for="item in queryResult.sentences">
+						<view>{{item.content}}</view>
+						<view>{{item.cn}}</view>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -31,8 +63,88 @@
 		onUnload,
 		onShow,
 	} from '@dcloudio/uni-app';
+	import { getTips, queryWord, queryWordHistory, queryWordAddCollect, queryWordRemoveCollect } from '@/utils/request.js'
 	
-	const tipsVisible = ref(false);
+	// 显示模块控制
+	const blockVisible = ref(1);
+	
+	// 搜索框内容
+	const queryWordValue = ref('')
+	
+	const blockVisibleConfig = {
+		history: 1,
+		tips: 2,
+		result: 3,
+	}
+	
+	const queryHistory = [
+		{word: 'click', desc: 'v.(在计算机屏幕上用鼠标)点击；豁然开朗；使咔嗒(或咔嚓)响；配合默契；被突然明白；（与某人）顷刻成为朋友'},
+		{word: 'clock', desc: 'n.时钟；钟'},
+		{word: 'good', desc: 'n.时钟；钟'},
+		{word: 'much', desc: 'n.时钟；钟'},
+		{word: 'positive', desc: 'adj.积极乐观的；完全的；良好的；正数的；建设性的；正电的；自信的；阳性的；拥护的；表示赞同的；有绝对把握；证据确凿的；朝着成功的'},
+		{word: 'active', desc: 'adj.积极乐观的；完全的；良好的；正数的；建设性的；正电的；自信的；阳性的；拥护的；表示赞同的；有绝对把握；证据确凿的；朝着成功的'},
+	]
+	
+	const queryResult = {
+		"result": "0",
+		"reason": "success",
+		"word": "knock",
+		"usRecUrl": "https://api.itso123.com/upload/words/k/knock.wav",
+		"ukRecUrl": "https://api.itso123.com/upload/words/k/knock.wav",
+		"trans": [
+			"n. 敲；敲打；爆震声",
+			"v. 敲；打；敲击"
+		],
+		"phrases": [
+			{
+				"content": "knock on",
+				"cn": "撞击撞出；敲击（门、窗）"
+			},
+			{
+				"content": "knock out",
+				"cn": "敲空；击倒；打破；使筋疲力竭"
+			}
+		],
+		"synos": [
+			{
+				"words": [
+					"strike",
+					"hit"
+				],
+				"cn": "vi. 敲；打；[动力]敲击"
+			},
+			{
+				"words": [
+					"strike",
+					"stroke",
+					"dong"
+				],
+				"cn": "vt. 敲；打；[动力]敲击；批评"
+			},
+			{
+				"words": [
+					"beating"
+				],
+				"cn": "n. 敲；敲打；[力]爆震声"
+			}
+		],
+		"sentences": [
+			{
+				"content": "I knocked and knocked but nobody answered.",
+				"cn": "我把门敲了又敲，但无人回应。"
+			}
+		]
+	}
+	
+	/**
+	 * 清除搜索框
+	 */
+	const clearSearchValue = () => {
+		console.log('清除搜索框')
+		queryWordValue.value = ''
+		blockVisible.value = blockVisibleConfig.history
+	}
 	
 	/**
 	 * 获取输入框
@@ -40,6 +152,7 @@
 	const inputTimer = ref(0)
 	const getInput = (e) => {
 		console.log('==获取查询输入框==', e.detail.value)
+		console.log('==获取查询输入框2==', queryWordValue.value)
 		const words = e.detail.value
 		
 		if (inputTimer.value) {
@@ -59,7 +172,7 @@
 	})
 	const dictQuery = (words) => {
 		if (!words) {
-			tipsVisible.value = false
+			blockVisible.value = blockVisibleConfig.history
 			return
 		}
 		const mockdata = [
@@ -67,10 +180,40 @@
 			{word: 'loose', desc: 'adj.未固定牢的，松动的；零散...'},
 		]
 		console.log('==开始查询==', words)
-		setTimeout(() => {
-			tipsVisible.value = true
+		setTimeout(async () => {
+			blockVisible.value = blockVisibleConfig.tips
 			tips.list = mockdata
+			// const tipResult = await getTips(words)
+			// console.log('tipResult', tipResult)
 			console.log('tips', tips)
+		}, 200)
+	}
+	
+	/**
+	 * 单词搜索
+	 */
+	const search = (word) => {
+		setTimeout(async () => {
+			blockVisible.value = blockVisibleConfig.result
+			console.log('单词搜索', word)
+			// tips.list = mockdata
+			// const tipResult = await getTips(words)
+			// console.log('tipResult', tipResult)
+			// console.log('tips', tips)
+		}, 200)
+	}
+	
+	/**
+	 * 历史记录搜索
+	 */
+	const searchByHistory = (word) => {
+		setTimeout(async () => {
+			blockVisible.value = blockVisibleConfig.result
+			console.log('历史单词搜索', word)
+			// tips.list = mockdata
+			// const tipResult = await getTips(words)
+			// console.log('tipResult', tipResult)
+			// console.log('tips', tips)
 		}, 200)
 	}
 </script>
@@ -94,10 +237,11 @@
 			height: 80rpx;
 			line-height: 80rpx;
 		}
-		.search-btn {
+		.search-clear-btn {
 			position: absolute;
 			right: 20rpx;
 			top: 20rpx;
+			z-index: 20;
 		}
 	}
 	.search-container {
@@ -124,7 +268,36 @@
 			margin-left: 40rpx;
 		}
 	}
+	.search-history {
+		.history-item {
+			padding: 20rpx 40rpx;
+			margin-top: 20rpx;
+			background: #e6f4ff;
+			margin-right: 20rpx;
+			border-radius: 10rpx;
+		}
+		.clear-btn {
+			margin-top: 40rpx;
+			padding: 20rpx;
+			text-align: center;
+		}
+	}
 	.search-result {
 		// border: 1px solid blueviolet;
+		.pronounce {
+			.speaker-icon {
+				width: 40rpx;
+				height: 40rpx;
+			}
+			padding: 20rpx 40rpx 40rpx;
+			border-bottom: 1px solid #dddddd;
+		}
+		.translate {
+			margin-top: 20rpx;
+			padding: 0 20rpx;
+		}
+		.phrases {
+			margin-top: 20rpx;
+		}
 	}
 </style>
--- utils/request.js
+++ utils/request.js
@@ -375,3 +375,55 @@ export const getTips = (word) => {
 		}
 	})
 }
+
+/**
+ * 单词查询
+ */
+export const queryWord = (word) => {
+	return request({
+		url: `/dict/search/precise`,
+		method: 'POST',
+		data: {
+			word
+		}
+	})
+}
+
+/**
+ * 查询历史记录
+ */
+export const queryWordHistory = (word) => {
+	return request({
+		url: `/dict/history/query`,
+		method: 'POST',
+		data: {
+			page: 1 //分页时使用，默认1为第1页
+		}
+	})
+}
+
+/**
+ * 搜藏单词
+ */
+export const queryWordAddCollect = (word) => {
+	return request({
+		url: `/dict/favorite/add`,
+		method: 'POST',
+		data: {
+			word
+		}
+	})
+}
+
+/**
+ * 取消单词搜藏
+ */
+export const queryWordRemoveCollect = (word) => {
+	return request({
+		url: `/dict/favorite/remove`,
+		method: 'POST',
+		data: {
+			word
+		}
+	})
+}
